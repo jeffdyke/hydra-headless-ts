3115ffcfc28afe5e90260e02d2f0fd91cf8e668b
Jeff Dyke <jeff.dyke@gmail.com>
2026-06-16 17:44:03 -0400

    build for arm and update docker to install ory-arm service

--- build/rebuild.sh
+++ build/rebuild.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -xe
-if [ "$1" = "force" ]; then
+hydra_running=$(docker ps --filter "name=hydra-headless-ts-1" -q) # Running or restarting, it needs to be stopped
+if [ ! -z "$hydra_running" ]; then
   sudo docker stop hydra-headless-ts-1
   sudo docker system prune -a -f
 fi
-docker build -f build/Dockerfile.headless-ts -t jeffdyke/hydra-headless-ts:latest .
+docker compose -f /src/hydra-headless-ts/docker-compose.yml build headless-ts
 docker push jeffdyke/hydra-headless-ts:latest
-sudo docker compose up -d --force-recreate headless-ts
+sudo docker compose -f /src/hydra-headless-ts/docker-compose.yml up -d --force-recreate headless-ts
