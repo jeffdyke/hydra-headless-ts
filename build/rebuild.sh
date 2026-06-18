c9ba7267b61fede8c777ae820bb5954c6bb95cd1
Jeff Dyke <jeff.dyke@gmail.com>
2026-06-18 11:13:24 -0400

    update build, still need to pass in branch and test

--- build/rebuild.sh
+++ build/rebuild.sh
@@ -2,6 +2,7 @@
 set -xe
 hydra_running=$(docker ps --filter "name=hydra-headless-ts-1" -q) # Running or restarting, it needs to be stopped
 BASE_IMAGE="668874212870.dkr.ecr.us-east-1.amazonaws.com/bondlink-hydra-headless-ts"
+COMPOSE_FILE="/src/hydra-headless-ts/docker-compose.yml"
 BUILD_DATE=$(date -u +"%Y%m%dT%H%M%S")
 GIT_BRANCH=$(git branch --show-current | tr '/' '-')
 GIT_COMMIT=$(git rev-parse --short HEAD)
@@ -10,9 +11,11 @@ if [ ! -z "$hydra_running" ]; then
   sudo docker stop hydra-headless-ts-1
   sudo docker system prune -a -f
 fi
-BRANCH_NAME=$GIT_BRANCH docker compose -f /src/hydra-headless-ts/docker-compose.yml build headless-ts
-#docker push jeffdyke/hydra-headless-ts:latest
-sudo docker compose -f /src/hydra-headless-ts/docker-compose.yml up -d --force-recreate headless-ts
+BRANCH_NAME=$GIT_BRANCH docker compose -f $COMPOSE_FILE build headless-ts
+
+sudo docker compose -f $COMPOSE_FILE up -d --force-recreate headless-ts
 
 docker tag $BASE_IMAGE:latest $BASE_IMAGE:$BUILD_HASH
+source ./login.sh
 docker push $BASE_IMAGE:$BUILD_HASH
+echo "Build and push complete: $BASE_IMAGE:$BUILD_HASH"
