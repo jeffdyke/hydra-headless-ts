8123ad54bf802595458814976aab90c4dbde632c
Jeff Dyke <jeff.dyke@gmail.com>
2026-06-21 17:35:05 -0400

    unsure

--- build/rebuild.sh
+++ build/rebuild.sh
@@ -15,10 +15,10 @@ if [ ! -z "$hydra_running" ]; then
   sudo docker stop hydra-headless-ts-1
   sudo docker system prune -a -f
 fi
-docker compose -f /src/hydra-headless-ts/docker-compose.yml --build --no-deps headless-ts
+sudo docker -f /src/hydra-headless-ts/docker-compose.yml --build --no-deps headless-ts
 
 if [ $IS_CI -eq 0 ]; then
-  sudo docker compose -f /src/hydra-headless-ts/docker-compose.yml up -d --force-recreate --no-deps headless-ts
+  sudo docker -f /src/hydra-headless-ts/docker-compose.yml up -d --force-recreate --no-deps headless-ts
 fi
 
 docker tag $BASE_IMAGE:latest $BASE_IMAGE:$BUILD_HASH
