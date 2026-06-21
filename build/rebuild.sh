c6f2585ef0292421e4fbe691b79cc864674eb472
Jeff Dyke <jeff.dyke@gmail.com>
2026-06-21 17:07:46 -0400

    update image path

--- build/rebuild.sh
+++ build/rebuild.sh
@@ -7,7 +7,7 @@ if [ "$1" = "--ci" ]; then
 else
   hydra_running=$(docker ps --filter "name=hydra-headless-ts-1" -q) # Running or restarting, it needs to be stopped
 fi
-BASE_IMAGE="668874212870.dkr.ecr.us-east-1.amazonaws.com/bondlink-hydra-headless-ts"
+BASE_IMAGE="668874212870.dkr.ecr.us-east-1.amazonaws.com/drone-hydra-headless-ts"
 BUILD_DATE=$(date -u +"%Y%m%dT%H%M%S")
 GIT_COMMIT=$(git rev-parse --short HEAD)
 BUILD_HASH="${BUILD_DATE}_hydra-headless-ts_${GIT_COMMIT}"
@@ -16,7 +16,7 @@ if [ ! -z "$hydra_running" ]; then
   sudo docker system prune -a -f
 fi
 docker compose -f /src/hydra-headless-ts/docker-compose.yml --build --no-deps headless-ts
-#docker push jeffdyke/hydra-headless-ts:latest
+
 if [ $IS_CI -eq 0 ]; then
   sudo docker compose -f /src/hydra-headless-ts/docker-compose.yml up -d --force-recreate --no-deps headless-ts
 fi
