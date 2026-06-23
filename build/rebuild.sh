aaba769732a801765eda8b783bf6e5526227e170
Jeff Dyke <jeff.dyke@gmail.com>
2026-06-23 14:51:51 -0400

    push images

--- build/rebuild.sh
+++ build/rebuild.sh
@@ -7,7 +7,9 @@ if [ "$1" = "--ci" ]; then
 else
   hydra_running=$(docker ps --filter "name=hydra-headless-ts-1" -q) # Running or restarting, it needs to be stopped
 fi
+COMPOSE_FILE="/src/hydra-headless-ts/docker-compose.yml"
 BASE_IMAGE="668874212870.dkr.ecr.us-east-1.amazonaws.com/drone-hydra-headless-ts"
+PUSH_IMAGE=${BASE_IMAGE/drone/bondlink}
 BUILD_DATE=$(date -u +"%Y%m%dT%H%M%S")
 GIT_COMMIT=$(git rev-parse --short HEAD)
 BUILD_HASH="${BUILD_DATE}_hydra-headless-ts_${GIT_COMMIT}"
@@ -17,14 +19,14 @@ if [ ! -z "$hydra_running" ]; then
   docker system prune -a -f
 fi
 echo "Docker $(which docker) version: $(docker --version)"
-docker compose -f /src/hydra-headless-ts/docker-compose.yml build headless-ts
+docker compose -f "${COMPOSE_FILE}" build headless-ts
 
 if [ $IS_CI -eq 0 ]; then
-  docker compose -f /src/hydra-headless-ts/docker-compose.yml up -d --force-recreate --no-deps headless-ts
+  docker compose -f "${COMPOSE_FILE}" up -d --force-recreate --no-deps headless-ts
 fi
 
 sudo docker compose -f "${COMPOSE_FILE}" up -d --force-recreate --no-deps headless-ts
 
-docker tag "${BASE_IMAGE}":latest "${BASE_IMAGE}":"${BUILD_HASH}"
-docker push "${BASE_IMAGE}":"${BUILD_HASH}"
-echo "Build and push complete: ${BASE_IMAGE}":"${BUILD_HASH}"
+docker tag "${BASE_IMAGE}":latest "${PUSH_IMAGE}":"${BUILD_HASH}"
+docker push "${PUSH_IMAGE}":"${BUILD_HASH}"
+echo "Build and push complete: ${PUSH_IMAGE}":"${BUILD_HASH}"
